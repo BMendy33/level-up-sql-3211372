470f2d262aa8a1eb196e149587ab9d831ac8d722
BMendy33 <59345590+BMendy33@users.noreply.github.com>
2022-12-13 16:39:35 +0000

    restaurant challenge 01 solution saved

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,8 @@
 -- Generate a list of customer information.
 
 -- Show their first name, last name, and email address.
+SELECT FirstName, LastName, Email 
+FROM Customers
+Order By LastName;
+
 -- Sort the list of results by last name.
